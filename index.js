1ed973852416ac3cb3d8d797754d57efaffc85f0
Blaise Iradukunda <iradub0@sewanee.edu>
2021-11-21 01:32:40 +0000

    pretty print

--- index.js
+++ index.js
@@ -20,25 +20,6 @@ const makeTable = (c, l, n) => {
   }
   return T
 }
-const makeTable_2 = (c, l, n) => {
-  var T = []
-  for (let i = 0; i < n; i++) {
-    T[i] = []
-  }
-  for (let i = 0; i < n; i++) {
-    n_char = 0
-    for (j = i; j < n; j++) {
-      T[i][j] = Number.POSITIVE_INFINITY
-      if (j === i)
-        n_char += c[j]
-      else
-        n_char += c[j] + 1
-      if (n_char <= l)
-        T[i][j] = Math.pow(l - n_char, 2)
-    }
-  }
-  return T
-}
 
 
 const pretty_print = (c, l, n) => {
@@ -62,26 +43,6 @@ const pretty_print = (c, l, n) => {
   }
   return { r, T }
 }
-const pretty_print_2 = (c, l, n) => {
-  var T = makeTable(c, l, n);
-  var r = Array(n)
-  r[n - 1] = T[n - 1][n - 1]
-  for (let i = 0; i <n; i++) {0
-    r[i] = T[i][n - 1];
-    for (j = 0; j < i; j++) {
-      // if(j==i)
-      //   n_char += c[j]
-      // else 
-      //   n_char += c[j]+1
-      // if(r[i] > r[j]){
-      // }
 
-      // chars[i] = l - n_char
-      r[i] = Math.min(T[i][j] + r[j+1], r[i])
-    }
-    console.log(r)
-  }
-  return { r, T }
-}
 
 console.log(pretty_print(C, l, C.length))
